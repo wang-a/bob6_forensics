ae066aae3cdeae797cfe24b3aee1da251152529f
wang-a <dnjy2002@gmail.com>
2017-07-16 01:09:01 -0700

    update

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import whois, sys
 
 urlf = sys.argv[1]
 f=open(urlf,'r')
-f2=open("jsonp.txt",'w')
-line=f.readline()
-domain = whois.query(line)
 
-f2.write(str(domain.__dict__))
-f2.close()
+while True:
+	line=f.readline()
+	domain = whois.query(line)
+	if not line : break
+	f2=open(line.replace("\n","") + ".json", 'w')
+	f2.write(str(domain.__dict__))
 f.close()
-
-
+f2.close()
